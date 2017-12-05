1131fe5199812d1a574e11bfa82f5a1b3ada484d
danielmoi <daniel@ampersandmoi.com>
2017-12-05 20:42:59 +1100

    Add emojis!

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -9,18 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var magicTableView: UITableView!
+    
+    var emojis = ["😎", "😡", "🤮", "👻", "💩", "😈", "😆"]
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return emojis.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
         let cell = UITableViewCell()
-        cell.textLabel?.text = "HELLO THERE"
+        cell.textLabel?.text = emojis[indexPath.row]
         return cell
     }
     
 
-    @IBOutlet weak var magicTableView: UITableView!
+    
     
     
     
